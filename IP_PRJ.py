92a34adec9d6a812133c32b3d87b317d99aef18e
Nam-Gan <164060612+Nam-Gan@users.noreply.github.com>
2024-05-30 11:10:52 +0530

    Add files via upload

---
+++ IP_PRJ.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import customtkinter as tk 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="namgandhi_3260",
+  database = "IP_PROJECT"
+)
+
+
+class SplashScreen:
+    def __init__(self, master):
+      self.master = master
+      #Frame
+      self.bars = tk.Frame(self.master, width = 200, height = 300)
+      self.bars.pack()
+       #Buttons
+      
+class Window:
+   def __init__(self,master) :
+      self.master = master
+cursor = mydb.cursor()
+root = tk.Tk()
+window = SplashScreen(root)
+root.mainloop()
