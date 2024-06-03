d5ef32d3b51d6d357e656ffee7584a8755b5924a
Nam-Gan <164060612+Nam-Gan@users.noreply.github.com>
2024-06-03 19:02:14 +0530

    Update IP_PRJ.py

--- IP_PRJ.py
+++ IP_PRJ.py
@@ -8,9 +8,54 @@ import inquirer
 #  password="pass",
 #  database = "IP_PROJECT"
 #)
+
+
+def BookName():
+    """Fetches data on the basis of the name of the book from SQL database, prints it (tabulated) and returns a list."""
+    name:str = typer.prompt("Enter the name of the Book")
+    cursor.execute(f"SELECT * FROM BOOKS WHERE NAME = {name}")
+    res = cursor.fetchone()
+    res = list[list]
+    Headers = ["NAME", "AUTHOTR", "ISBN", "DATE OF PUBLISHING","TAGS", "STATUS", "TOTAL"]
+    print(tabulate(res, headers = Headers))
+    return res
+
+
+def ISBN():
+    isbn:int = typer.prompt("Enter the ISBN ID of the Book:")
+    cursor.execute(f"SELECT * FROM BOOKS WHERE ISBN = {isbn}")
+    res = cursor.fetchone()
+    res = list(res)
+    Headers = ["NAME", "AUTHOTR", "ISBN", "DATE OF PUBLISHING","TAGS", "STATUS", "TOTAL"]
+    print(tabulate(res, headers = Headers))
+    return res
+
+def Search(parent):
+    ch2  = inquirer.list_input("Search By:" ,choices = ["BookName", "ISBN", "Author", "DateOfPublishing", "back"])
+    if ch2 == "back":
+        f2 = globals()[parent]
+        f2()
+    else:
+        f2 = globals()[ch2]
+        f2()
+
+
+def Nofilter():
+    cursor.execute("SELECT * FROM customers")
+    res = cursor.fetchall()
+    res = list(res)
+    Headers = ["NAME", "AUTHOTR", "ISBN", "DATE OF PUBLISHING","TAGS", "STATUS", "TOTAL"]
+    print(tabulate(res, headers = Headers))
+
+
 def Library():
     #Logic for Library
-    
+    choice1 = inquirer.list_input("Filters? or Nah!", choices = ["NoFilter","Search", "Date", "Tags","back"])
+    if choice1 == "back":
+        main()
+    else:
+        func1 = globals()[choice1]
+        func1("Library")
     pass
 def AddBook():
     #Logic to Add book
@@ -29,6 +74,7 @@ def main():
     func = globals()[que]
     func()
 if __name__ == "__main__":
-    #cursor = mydb.cursor()
+    cursor = mydb.cursor()
     typer.run(main)
     pass
+
