12db996605d6d16fd067061ef2eb05b61aaa298f
Nam-Gan <164060612+Nam-Gan@users.noreply.github.com>
2024-06-03 17:56:40 +0530

    Update IP_PRJ.py

--- IP_PRJ.py
+++ IP_PRJ.py
@@ -1,25 +1,34 @@
 import mysql.connector
-import customtkinter as tk 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="pass",
-  database = "IP_PROJECT"
-)
-
-
-class SplashScreen:
-    def __init__(self, master):
-      self.master = master
-      #Frame
-      self.bars = tk.Frame(self.master, width = 200, height = 300)
-      self.bars.pack()
-       #Buttons
-      
-class Window:
-   def __init__(self,master) :
-      self.master = master
-cursor = mydb.cursor()
-root = tk.Tk()
-window = SplashScreen(root)
-root.mainloop()
+import typer
+import tabulate
+import inquirer
+#mydb = mysql.connector.connect(
+#  host="localhost",
+#  user="root",
+#  password="pass",
+#  database = "IP_PROJECT"
+#)
+def Library():
+    #Logic for Library
+    
+    pass
+def AddBook():
+    #Logic to Add book
+    pass
+def RemoveBook():
+    #Logic to remove book
+    pass
+def IssueBook():
+    #Logic to Issue book
+    pass
+def ReturnBook():
+    #Logic to Return book
+    pass
+def main():
+    que = inquirer.list_input("What do you want to do?", choices = [Library.__name__, AddBook.__name__, RemoveBook.__name__, IssueBook.__name__, ReturnBook.__name__])
+    func = globals()[que]
+    func()
+if __name__ == "__main__":
+    #cursor = mydb.cursor()
+    typer.run(main)
+    pass
